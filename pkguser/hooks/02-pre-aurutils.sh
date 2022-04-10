947cad59209f2bd86bce9bd427d845912a21eaf7
Alejandro Ojeda Gutiérrez <63747287+alexojegu@users.noreply.github.com>
2022-04-10 13:07:45 +0200

    Update aurutils repository

--- pkguser/hooks/02-pre-aurutils.sh
+++ pkguser/hooks/02-pre-aurutils.sh
@@ -4,7 +4,7 @@ sudo tee -a "/etc/pacman.conf" << EOF
 
 [aurutilsci]
 SigLevel = Optional TrustAll
-Server = https://github.com/xeladejo/aurutilsci/releases/download/repository
+Server = https://github.com/alexojegu/aurutilsci/releases/download/repository
 EOF
 
 sudo pacman -Syu --needed --noconfirm aurutils
